c1c8a39b2716421f6bd8ec5b38f069eae60a53ee
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-20 01:46:22 +0300

    Break keyword added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/LoopControlStatements/BreakKeyword.kt
@@ -0,0 +1,23 @@
+package LoopControlStatements
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 01:46          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    for (i in 0..10) {
+        println(i)
+
+        if (i == 5) {
+            break
+        }
+    } //print 0 1 2 3 4 5
+}

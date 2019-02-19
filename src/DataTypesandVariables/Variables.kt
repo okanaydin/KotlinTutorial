c4a9b4fbd851009be1e9562341421171a4a4ea68
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-20 01:25:04 +0300

    Comments added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/DataTypesandVariables/Variables.kt
@@ -0,0 +1,32 @@
+package DataTypesandVariables
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 01:23          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var name: String = "Okan"
+
+    var surname: String = "AYDIN"
+
+    var age: Int = 24
+
+    var charValue: Char = 'A'
+
+    var doubleValue: Double = 20.0
+
+    var isFlag: Boolean = false
+
+    println("your name: $name")
+
+    println("your name and age: $name $surname ${age.toDouble()}")
+
+}

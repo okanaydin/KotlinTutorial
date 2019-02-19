500970c0d987a758215192ad1f7cb2a77e989180
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-20 01:45:06 +0300

    While loop added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/LoopControlStatements/WhileLoop.kt
@@ -0,0 +1,22 @@
+package LoopControlStatements
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 01:44          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var i: Int = 1
+
+    while (i <= 10) {
+        println("i:" + i)
+        i++
+    }
+}

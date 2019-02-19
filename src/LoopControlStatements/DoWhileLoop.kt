477221b830718d8c91eb1262ba01e444e2e409cd
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-20 01:45:43 +0300

    Do-While loop added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/LoopControlStatements/DoWhileLoop.kt
@@ -0,0 +1,23 @@
+package LoopControlStatements
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 01:45          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var k = 0
+
+    do {
+        println("k:" + k)
+        k += 2
+    } while (k <= 10)
+
+}

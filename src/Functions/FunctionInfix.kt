c0043edfc44a40d9c9458cb460412ba456b3f3c2
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-20 16:06:54 +0300

    Named Parameters added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/Functions/FunctionInfix.kt
@@ -0,0 +1,30 @@
+package Functions
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 15:51          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var fullName = "Okan " add "Aydın"
+    println(fullName)
+
+    var sum = 2 add2 5
+    println(sum)
+
+}
+
+//infix functions example-1
+infix fun String.add(name: String): String = this + name
+
+//infix functions example-2
+infix fun Int.add2(number: Int): Int = this + number
+
+
---
+++ src/Functions/NamedParameters.kt
@@ -0,0 +1,26 @@
+package Functions
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 20.02.2019 - 16:02          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var result = calculateArea(length = 2, width = 5)
+
+    print(result)
+
+}
+
+fun calculateArea(length: Int, width: Int, height: Int = 10): Int {
+
+    return length * width * height
+}
+

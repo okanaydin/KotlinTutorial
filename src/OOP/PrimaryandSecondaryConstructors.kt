7e9d0c51d5caf839c76465f198ded8d57b5a7261
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-02-24 20:52:41 +0300

    Primary and secondary constructors added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/OOP/PrimaryandSecondaryConstructors.kt
@@ -0,0 +1,37 @@
+package OOP
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 24.02.2019 - 20:21          │
+└─────────────────────────────┘
+ */
+
+
+//primary constructor
+class News constructor(newsID: Int) {
+
+    //secondary constructor
+    constructor(
+        newsID: Int,
+        newsURL: String,
+        newsTitle: String
+    ) : this(1000)
+
+}
+
+fun main() {
+
+    val newsInstance1: News = News(1001)
+
+    val newsIntance2: News? = null
+
+    val newsIntance3: News = News(1002, "Magazine", "Bob id dead")
+
+    val newsIntance4: News = News(newsID = 1002, newsURL = "Magazine", newsTitle = "Bob id dead")
+
+}

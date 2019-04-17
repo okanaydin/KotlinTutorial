fcbbac98b971e431f5aa8e2bc89c5bc54e86f282
Okan AYDIN <okan.aydin@isik.edu.tr>
2019-04-17 11:52:17 +0300

    List example added.
    Signed-off-by: Okan AYDIN <okan.aydin@isik.edu.tr>

---
+++ src/CollectionsProgramming/List.kt
@@ -0,0 +1,21 @@
+package CollectionsProgramming
+
+
+/** Code with ❤
+┌─────────────────────────────┐
+│ Created by Okan AYDIN       │
+│ ─────────────────────────── │
+│ okan.aydin@isik.edu.tr      │
+│ ─────────────────────────── │
+│ 17.04.2019 - 11:46          │
+└─────────────────────────────┘
+ */
+
+fun main() {
+
+    var myList = listOf(0, 1, 2, 3, 4, "Okan", "Aydin", "Hasan", "Ayse", "Fatma")
+
+    for (index in 0 until myList.size) println(myList[index])
+
+
+}
